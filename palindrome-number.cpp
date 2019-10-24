e5fd5ba53b072b4e0bceee9903e742e38f672858
rice-cake <3152334+rice-cake@users.noreply.github.com>
2019-10-24 10:12:25 -0400

    Add files via upload

---
+++ palindrome-number.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+//rice-cake
+
+/*
+Problem (Easy):
+
+Determine whether an integer is a palindrome. An integer is a palindrome when it reads 
+the same backward as forward.
+
+Example 1: Input: 121 Output: true
+
+Example 2: Input: -121 Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. 
+Therefore it is not a palindrome.
+
+Example 3: Input: 10 Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+Follow up: Coud you solve it without converting the integer to a string?
+*/
+
+//Solution status: Accepted
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false;
+        vector<short> v, rv;        
+        while (x > 0) {
+            v.push_back(x % 10);            
+            x = x / 10;
+        }
+        rv = v;
+        reverse(rv.begin(), rv.end());
+        return(v == rv);
+    }
+};
+
+int main() {
+  string res[2] = { "false", "true" };
+  Solution s;
+  cout << res[s.isPalindrome(121)] << endl;  
+  cout << res[s.isPalindrome(-121)] << endl;  
+  cout << res[s.isPalindrome(10)] << endl;  
+
+  char c; cin >> c;
+  return 0;
+}
+
---
+++ two-sum.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+// #include <initializer_list>
+using namespace std;
+
+//rice-cake
+
+/*
+Problem (Easy):
+Given an array of integers, return indices of the two numbers such that 
+they add up to a specific target. 
+You may assume that each input would have exactly one solution, 
+and you may not use the same element twice.
+
+Example:
+Given nums = [2, 7, 11, 15], target = 9,
+Because nums[0] + nums[1] = 2 + 7 = 9, return [0, 1].
+*/
+
+//Solution status: Accepted
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> res(2, -1);
+        int sz = nums.size();
+        for (int i =0; i < sz-1; ++i) {
+            for (int j = i+1; j < sz; ++j) {
+                if (nums.at(i) + nums.at(j) == target) {
+                    res.at(0) = i; res.at(1) = j;
+                    return res;
+                }
+            }
+        }        
+        return res; 
+    }
+};
+
+int main() {
+  int target = 9;
+  auto nums_il = { 2, 7, 11, 15 };     
+  vector<int> nums_v(nums_il);  
+  Solution s;
+  vector<int> res_v = s.twoSum(nums_v, target);
+  cout << res_v[0] << " " << res_v[1] << endl;  
+  char c; cin >> c;
+  return 0;
+}
